742b9e4b56b8f2f827bf61bfa7cc1e685923f32f
bennysun <bennysun.dev@gmail.com>
2024-05-19 17:40:47 +0800

    init: vehicles api

---
+++ vehicles-api/src/test/java/com/udacity/jwdnd/c2/vehiclesapi/VehiclesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.c2.vehiclesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
