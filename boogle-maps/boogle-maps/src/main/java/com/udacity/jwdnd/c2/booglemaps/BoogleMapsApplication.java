20568dd4ee82d76bb23d06758475e414a1d6473f
bennysun <bennysun.dev@gmail.com>
2024-05-20 15:40:56 +0800

    config: register eureka client

--- boogle-maps/boogle-maps/src/main/java/com/udacity/jwdnd/c2/booglemaps/BoogleMapsApplication.java
+++ boogle-maps/boogle-maps/src/main/java/com/udacity/jwdnd/c2/booglemaps/BoogleMapsApplication.java
@@ -2,8 +2,10 @@ package com.udacity.jwdnd.c2.booglemaps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BoogleMapsApplication {
 
 	public static void main(String[] args) {
